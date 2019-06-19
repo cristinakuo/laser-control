6a003f00da1acb228956d7009893737090230d21
Cristina <ckuo@fi.uba.ar>
2019-06-19 14:28:24 -0300

    Add

--- stepper-control/src/main.cpp
+++ stepper-control/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <math.h>
+#include "Keypad.h"
 #include "motor.hpp"
 
 #define BAUDRATE 9600 // Serial communication
@@ -17,6 +18,19 @@
 #define TARGET_MS1_PIN 50 // DEBUG: En realidad no sirven para nada
 #define TARGET_MS2_PIN 51
 #define TARGET_MS3_PIN 52
+
+// Keypad
+#define KEYPAD_R1_PIN 53
+#define KEYPAD_R2_PIN 51
+#define KEYPAD_R3_PIN 49
+#define KEYPAD_R4_PIN 47
+#define KEYPAD_C1_PIN 45
+#define KEYPAD_C2_PIN 43
+#define KEYPAD_C3_PIN 41
+#define KEYPAD_C4_PIN 39
+
+#define KEYPAD_ROWS 4
+#define KEYPAD_COLS 4
 
 // Interrupt pins
 #define BUTTON_PIN 2 // Start/Stop button
@@ -42,6 +56,19 @@ long unsigned timeTable[ARRAY_MAX_LEN];
 size_t table_size; // TODO: ver que onda cuando haya otra tabla 
 long unsigned initial_time; // [us]
 
+// Keypad variables
+byte rowPins[KEYPAD_ROWS] = {KEYPAD_R1_PIN, KEYPAD_R2_PIN, KEYPAD_R3_PIN, KEYPAD_R4_PIN};
+byte colPins[KEYPAD_COLS] = {KEYPAD_C1_PIN, KEYPAD_C2_PIN, KEYPAD_C3_PIN, KEYPAD_C4_PIN}; 
+
+
+char hexaKeys[KEYPAD_ROWS][KEYPAD_COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS); 
 void setup() {
 	// Init serial communication
     Serial.begin(BAUDRATE);
@@ -72,6 +99,7 @@ void setup() {
     wait_to_start();
     // Stop program if button is hit 
     attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), button_ISR, RISING);
+
     initial_time = micros();
     carrito.init();
     target.init();
